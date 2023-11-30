95df7b59f395542773d200a45393517831b4376d
Wilker <wilker22@outlook.com>
2023-11-30 17:07:35 -0300

    ajustes no sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,27 +9,27 @@
 
         <ul class="sidebar-menu">
 
-            <li class="active"><a class="nav-link" href="{{route('admin_home')}}" 
+            <li class="{{ Request::is('admin/home') ? 'active' : '' }}"><a class="nav-link" href="{{route('admin_home')}}" 
                 data-bs-toggle="tooltip" 
                 data-bs-placement="rigth" data-bs-title="Dashboard"><i class="fas fa-hand-point-right"></i>
                     <span>Dashboard</span></a></li>
 
-            <li class="nav-item dropdown active">
+            <li class="nav-item dropdown {{ Request::is('admin/home-page') ? 'active' : '' }}">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i>
                     <span>Configurações de Páginas</span></a>
                 <ul class="dropdown-menu">
-                    <li class="active"><a class="nav-link" href="{{ route('admin_home_page') }}"><i class="fas fa-angle-right"></i> Home</a>
+                    <li class="{{ Request::is('admin/home-page') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin_home_page') }}"><i class="fas fa-angle-right"></i> Home</a>
                     </li>
                     <li class=""><a class="nav-link" href="{{ route('terms')}}"><i class="fas fa-angle-right"></i> Terms</a>
                     </li>
                 </ul>
             </li>
 
-            <li class="nav-item dropdown active">
+            <li class="nav-item dropdown {{ Request::is('admin/job-category/*') ? 'active' : '' }}">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i>
                     <span>Vagas</span></a>
                 <ul class="dropdown-menu">
-                    <li class="active"><a class="nav-link" href="{{ route('admin_job_category') }}"><i class="fas fa-angle-right"></i> Categorias</a>
+                    <li class="{{ Request::is('admin/job-category/*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin_job_category') }}"><i class="fas fa-angle-right"></i> Categorias</a>
                     </li>
                     <li class=""><a class="nav-link" href="{{ route('admin_job_location')}}"><i class="fas fa-angle-right"></i> Localizações</a>
                     </li>
